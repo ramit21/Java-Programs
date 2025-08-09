84142a007e1c5bf3648d9f3c0c654fea2139f10a
ramit21 <21.ramit@gmail.com>
2025-08-09 19:33:35 +0100

    .

--- src/main/java/code/HttpClientExample.java
+++ src/main/java/code/HttpClientExample.java
@@ -1,18 +1,25 @@
 package code;
 
 
-import org.json.simple.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.net.URI;
-import java.net.http.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class HttpClientExample {
 
     /*
         Call this url using Httpclient and return list of email ids of the people who have commented on the given post.
         https://jsonplaceholder.typicode.com/comments?postId=1
-        Process the response using JSONArray and JSONObject.
+        Process the response using JSONArray and JSONObject, or using ObjectMapper
         Note important imports above.
      */
     public static void main(String[] args) {
@@ -31,10 +38,26 @@ public class HttpClientExample {
             for (Object jsonObj : jsonArray) {
                 System.out.println(((JSONObject) jsonObj).get("email"));
             }
+
+            //Read into pojo
+            ObjectMapper mapper = new ObjectMapper(); //Import jackson bind
+            Comment[] comments = mapper.readValue(response.body(), Comment[].class);
+            System.out.println(comments[0]);
         } catch (Exception ex) {
             System.out.println(ex);
         }
 
     }
 
+}
+
+@Value
+@Builder
+@Jacksonized
+class Comment {
+    private Long postId;
+    private Long id;
+    private String name;
+    private String email;
+    private String body;
 }
